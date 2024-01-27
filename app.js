e102da67427664bd25cd2c28cf638e5882e3b52e
samade747 <samad.e747@gmail.com>
2024-01-27 12:31:14 +0500

    1231

---
+++ app.js
@@ -0,0 +1,61 @@
+// Assignment 3 
+
+// const temperatureCelsius = +prompt('Enter the current temperature in Celsius:');
+
+// if (temperatureCelsius > 30) {
+//     alert("Oh ====> It's a hot day!");
+// } else {
+//     alert("===> The weather is moderate.");
+// }
+
+
+// Assignment 3 b
+
+
+// const Maths = +prompt('Enter Marks of Maths');
+// const English = +prompt('Enter Marks of Maths');
+// const urdu = +prompt('Enter Marks of Maths');
+
+// const totalMarks = Maths + English + urdu;
+// const percentage = (totalMarks / 300) * 100;
+
+// let userGrade;
+
+
+// if (percentage >= 90) {
+//     userGrade = 'A+';
+// } else if (percentage >= 80) {
+//     userGrade = 'A';
+// } else if (percentage >= 70) {
+//     userGrade = 'B';
+// } else if (percentage >= 60) {
+//     userGrade = 'C';
+// } else if (percentage >= 50) {
+//     userGrade = 'D';
+// } else {
+//     userGrade = 'Fail';
+// }
+
+// document.write(`--------- Marksheet --------- <hr> <br> Subject Maths: "  ${Maths} <br>  Subject English: " ${English} <br> Subject Urdu: "  ${urdu} <br> Total Marks: "  ${totalMarks}  <br> Percentage: ${percentage.toFixed(2)}% <br> <hr>  Grade:   ${userGrade} <br> <hr> <hr> .`  );
+// document.write(`Subject 1: "  ${Maths}`);
+// document.write(`Subject 1: "  ${English}`);
+// document.write(`Subject 1: "  ${urdu}`);
+// document.write(`Total Marks: "  ${totalMarks}`);
+// document.write(`Percentage: ${percentage.toFixed(2)}%`)
+// document.write(`Grade:   ${userGrade}`)
+
+
+// Assignment 4a
+
+// const number = +prompt("Enter a number of table:");
+// const a = +prompt("Enter a number of table start:");
+// const e = +prompt("Enter a number of table end:");
+
+// document.write(`<h2>Multiplication Table for ${number}</h2>`);
+// document.write("<ul>");
+// for (let i = a; a <= e; a++) {
+//     const result = number * i;
+//     document.write(`<li>${number} x ${i} = ${result}</li>`);
+// }
+// document.write("</ul>");
+
